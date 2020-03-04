8d6588ea4a45ea1e0f726725856341ed3281cc1b
mashimi <lmashimi@gmail.com>
2020-03-05 00:50:22 +0100

    Updates

---
+++ precache-manifest.f4d53fd301436e0af3480eed26d9e3c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79660cadbb5d6e0f87e4d04d3df32f82",
+    "url": "/finalcode/index.html"
+  },
+  {
+    "revision": "1426da1cfb2cb2f631e5",
+    "url": "/finalcode/static/css/2.e89d6abc.chunk.css"
+  },
+  {
+    "revision": "b5d2beeaa6d9f0e53138",
+    "url": "/finalcode/static/css/main.0452d5d9.chunk.css"
+  },
+  {
+    "revision": "1426da1cfb2cb2f631e5",
+    "url": "/finalcode/static/js/2.d724599a.chunk.js"
+  },
+  {
+    "revision": "10cd4d51e488e1d0f90277c18b3de8b5",
+    "url": "/finalcode/static/js/2.d724599a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5d2beeaa6d9f0e53138",
+    "url": "/finalcode/static/js/main.26cf270d.chunk.js"
+  },
+  {
+    "revision": "79d6975683ecaf63609c",
+    "url": "/finalcode/static/js/runtime-main.10c57efb.js"
+  }
+]);
